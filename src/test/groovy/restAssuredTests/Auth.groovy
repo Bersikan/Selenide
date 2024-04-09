9453ab2825c3015c69b616899cae080471acfefc
Bersikan <khozeyev@gmail.com>
2024-04-09 20:41:25 +0300

    main 555

--- src/test/groovy/restAssuredTests/Auth.groovy
+++ src/test/groovy/restAssuredTests/Auth.groovy
@@ -10,7 +10,7 @@ class Auth extends CommonBaseSpecification {
         Map payload = [
                 userId           : "a178a104-e3c0-4f50-bbd5-3e4919f063e8",
                 collectionOfIsbns: [[
-                                            isbn: "9781449325862"
+                                            isbn: "9781449325863"
                                     ]]
         ]
 
