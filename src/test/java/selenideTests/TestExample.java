9e3aa82ebcece6a1212f288e933574f899a84aff
Bersikan <khozeyev@gmail.com>
2024-04-15 17:36:40 +0300

    Jenkins props

--- src/test/java/selenideTests/TestExample.java
+++ src/test/java/selenideTests/TestExample.java
@@ -3,21 +3,17 @@ package selenideTests;
 import general_helpers.element.Element;
 import general_helpers.element.ElementBuilder;
 import org.openqa.selenium.By;
-import org.testng.Assert;
-import testNG.group_annotations.IntegrationTest;
-import testNG.group_annotations.SmokeTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import selenideConfig.BaseConfig;
-
-import java.util.Objects;
+import testNG.group_annotations.IntegrationTest;
+import testNG.group_annotations.SmokeTest;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static git_hub_selenide.data.ErrorMessages.INCORRECT_LOGIN_MESSAGE;
 import static general_helpers.StringHelper.randomAlpha;
+import static git_hub_selenide.data.ErrorMessages.INCORRECT_LOGIN_MESSAGE;
 import static pages.main.gitHubLogin.*;
 
 public class TestExample extends BaseConfig {
@@ -31,10 +27,6 @@ public class TestExample extends BaseConfig {
     @SmokeTest
     public void incorrectLogIn() {
         String browser = System.getenv("browser");
-        if(!Objects.equals(browser, "chrome"))
-        {
-            Assert.fail("asdasdasd");
-        }
         logIn(randomAlpha(10), randomAlpha(10));
         errorFrame.shouldHave(text(INCORRECT_LOGIN_MESSAGE));
         buttonErrorFrameClose.click();
